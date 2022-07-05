a2aec67cff46967a4affea3f6c95e57ec0123283
Megan Smith <msmith@ag.com>
2022-07-05 18:33:52 -0400

    search form component and useState Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Lillian" animal="Cat" breed="Tortie"></Pet>
-      <Pet name="Quirgie" animal="Dog" breed="Corgi"></Pet>
-      <Pet name="Doffu" animal="Deer" breed="Tuan"></Pet>
+      <SearchParams />
     </div>
   );
 };
