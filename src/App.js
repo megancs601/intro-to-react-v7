fafee633fa3c6e00f286210a999302957b5c5a4e
Megan Smith <msmith@ag.com>
2022-07-05 17:58:22 -0400

    configure eslint with jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Lillian",
-      animal: "Cat",
-      breed: "Tortie",
-    }),
-    React.createElement(Pet, {
-      name: "Quirgie",
-      animal: "Dog",
-      breed: "Corgi",
-    }),
-    React.createElement(Pet, {
-      name: "Doffu",
-      animal: "Deer",
-      breed: "Tuan",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Lillian" animal="Cat" breed="Tortie"></Pet>
+      <Pet name="Quirgie" animal="Dog" breed="Corgi"></Pet>
+      <Pet name="Doffu" animal="Deer" breed="Tuan"></Pet>
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
