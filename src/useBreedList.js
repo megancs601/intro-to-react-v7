cafead6b0c5d42ff4d42589e3b622aa4d1e3b330
Megan Smith <msmith@ag.com>
2022-07-07 14:24:57 -0400

    custom hooks

---
+++ src/useBreedList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const localCache = {};
+//custom hook
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  //not needed, but may be useful to let users know what the status is with a loading graphic
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList;
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
